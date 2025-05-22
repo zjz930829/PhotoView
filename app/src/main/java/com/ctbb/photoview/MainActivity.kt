1fa79f04e942617959ee3a35b86a050d040461a2
xndm/zjz <zhongjingzhong@xndm.tech>
2025-05-22 15:38:04 +0800

    提交

---
+++ app/src/main/java/com/ctbb/photoview/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ctbb.photoview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
